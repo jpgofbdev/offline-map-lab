9a14e52ad32e914451a862ab5f531f078e484964
jpgofbdev <jpgofbdev@gmail.com>
2026-02-13 17:08:59 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
+const BASE = self.location.pathname.replace(/sw\.js$/, '');
+
 const SHELL_ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./offline.js",
-  "./regions.json",
-  "./style.json",
-  "./maplibre-gl.js",
-  "./maplibre-gl.css",
-  "./pmtiles.js"
+  BASE,
+  BASE + "index.html",
+  BASE + "style.css",
+  BASE + "offline.js",
+  BASE + "regions.json",
+  BASE + "style.json",
+  BASE + "maplibre-gl.js",
+  BASE + "maplibre-gl.css",
+  BASE + "pmtiles.js"
 ];
